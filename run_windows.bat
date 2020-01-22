197a6667f6427bbc0d4a7933a25c8341f96121e4
Nico Zevallos <gnastacast@gmail.com>
2020-01-21 20:49:00 -0500

    Added script to automatically run on Windows

---
+++ run_windows.bat
@@ -0,0 +1,5 @@
+setlocal
+cd /d %~dp0
+start chrome --kiosk http://127.0.0.1:5000 --incognito --disable-pinch --overscroll-history-navigation=0 --disable-features=CrossSiteDocumentBlockingAlways,CrossSiteDocumentBlockingIfIsolating
+rem start chrome http://127.0.0.1:5000  --disable-features=CrossSiteDocumentBlockingAlways,CrossSiteDocumentBlockingIfIsolating
+C:\Users\%USERNAME%\Anaconda3\Scripts\activate.bat gpt-2 && cd /d flask && python application.py
