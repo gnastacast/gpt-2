be595bfa24ea123b335808260534f27345746322
Nico Zevallos <gnastacast@gmail.com>
2020-01-21 21:12:45 -0500

    Added script to run on linux

---
+++ run_unix.sh
@@ -0,0 +1,11 @@
+cd -P -- "$(dirname -- "$0")"
+if [ ! -d "env" ] 
+then
+	virtualenv env
+	source env/bin/activate
+	pip install -r requirements.txt
+fi
+source env/bin/activate
+/usr/bin/google-chrome --window-size=1920,1080 --kiosk --window-position=0,0 http://127.0.0.1:5000  --incognito --disable-pinch --overscroll-history-navigation=0 --disable-features=CrossSiteDocumentBlockingAlways,CrossSiteDocumentBlockingIfIsolating &
+python flask/application.py 
+
