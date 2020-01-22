d68e22fbcaf3ba7fb47108d847d704964f8f1566
Nico Zevallos <gnastacast@gmail.com>
2020-01-22 10:48:55 -0500

    Fixed error with && symbol in run script

--- run_unix.sh
+++ run_unix.sh
@@ -1,14 +1,7 @@
 cd -P -- "$(dirname -- "$0")"
 if [ ! -d "env" ] 
 then
-	virtualenv env
-	if [ -d "env" ] 
-	then
-		source env/bin/activate
-		pip install -r requirements.txt
-	fi
+	virtualenv env && pip install -r requirements.txt
 fi
-source env/bin/activate
-/usr/bin/google-chrome --window-size=1920,1080 --kiosk --window-position=0,0 http://127.0.0.1:5000  --incognito --disable-pinch --overscroll-history-navigation=0 --disable-features=CrossSiteDocumentBlockingAlways,CrossSiteDocumentBlockingIfIsolating &
-python flask/application.py 
-
+source env/bin/activate && /usr/bin/google-chrome --window-size=1920,1080 --kiosk --window-position=0,0 http://127.0.0.1:5000  --incognito --disable-pinch --overscroll-history-navigation=0 --disable-features=CrossSiteDocumentBlockingAlways,CrossSiteDocumentBlockingIfIsolating &
+source env/bin/activate && python flask/application.py 
