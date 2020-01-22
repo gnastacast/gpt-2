b1df69496fcc9ecf304f3c9609762f27618bbd6d
Nico Zevallos <gnastacast@gmail.com>
2020-01-22 00:01:59 -0500

    Made run unix a little bit safer for when virtualenv is not installed

--- run_unix.sh
+++ run_unix.sh
@@ -2,8 +2,11 @@ cd -P -- "$(dirname -- "$0")"
 if [ ! -d "env" ] 
 then
 	virtualenv env
-	source env/bin/activate
-	pip install -r requirements.txt
+	if [ -d "env" ] 
+	then
+		source env/bin/activate
+		pip install -r requirements.txt
+	fi
 fi
 source env/bin/activate
 /usr/bin/google-chrome --window-size=1920,1080 --kiosk --window-position=0,0 http://127.0.0.1:5000  --incognito --disable-pinch --overscroll-history-navigation=0 --disable-features=CrossSiteDocumentBlockingAlways,CrossSiteDocumentBlockingIfIsolating &
